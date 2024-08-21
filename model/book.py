87e7697af8ee3beef62f1b701593c72289d0166d
Rrrrry123 <ryantrujillo@comcast.net>
2024-08-21 10:03:23 -0600

    Begin work on adding events.

--- model/book.py
+++ model/book.py
@@ -1,13 +1,58 @@
+from typing import Callable, Final
+
+
 class Book:
+    BUILT_IN_PROPERTIES: Final[list[str]] = ["_title", "_author", "_publication_year"]
+    BUILT_IN_PROPERTIES_DISPLAY: Final[list[str]] = ["Title", "Author", "Publication Year"]
+
     def __init__(self, title: str, author: str, publication_year: str, custom_properties: list[str]):
         self.id = -1
-        self.title = title
-        self.author = author
-        self.publication_year = publication_year
+        self._title = title
+        self._author = author
+        self._publication_year = publication_year
         self.custom_properties: dict[str, str] = {}
         for custom_property in custom_properties:
             self.custom_properties[custom_property] = ""
+        self.observers: list[Callable[[str], None]] = []
 
-    def define_custom_property(self, **kwargs):
-        for property in kwargs.keys():
-            self.custom_properties[property] = kwargs[property]
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value: str):
+        self._title = value
+        self._notify_property_changed("Title")
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, value: str):
+        self._author = value
+        self._notify_property_changed("Author")
+
+    @property
+    def publication_year(self):
+        return self._publication_year
+
+    @publication_year.setter
+    def publication_year(self, value: str):
+        self._publication_year = value
+        self._notify_property_changed("Publication Year")
+
+    def define_custom_property(self, **kwargs: str):
+        for property, value in kwargs.items():
+            self.custom_properties[property] = value
+            self._notify_property_changed(property)
+
+    def remove_custom_property(self, key: str):
+        try:
+            del self.custom_properties[key]
+        except KeyError as e:
+            print(f"Attempting to delete property {key} from book {self.id}. Property does not exist.\n{repr(e)}")
+
+    def _notify_property_changed(self, property_name: str):
+        for observer in self.observers:
+            observer(property_name)
--- model/booklist.py
+++ model/booklist.py
@@ -1,34 +1,54 @@
 from model.book import Book
+from typing import Callable, Final
 
 
 class Booklist:
     def __init__(self, name: str, is_user_created: bool, books: list[Book]):
-        self.name = name
-        self.books = books
-        self.is_user_created = is_user_created
+        self._name = name
+        self._books = books
+        self.IS_USER_CREATED: Final[bool] = is_user_created
+        self.observers: list[Callable[[str], None]] = []
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value: str):
+        self._name = value
+        self._notify_property_changed("name")
+
+    def add_book(self, book: Book):
+        self._books.append(book)
+        self._notify_property_changed("books")
+
+    def remove_book_by_id(self, id: int):
+        for book in self._books:
+            if id == book.id:
+                self._books.remove(book)
+                self._notify_property_changed("books")
 
     def get_book_by_id(self, id: int) -> Book | None:
-        for book in self.books:
+        for book in self._books:
             if id == book.id:
                 return book
         return None
     
     def has_book(self, id: int) -> bool:
-        for book in self.books:
+        for book in self._books:
             if id == book.id:
                 return True
         return False
-    
-    def remove_book_by_id(self, id: int):
-        for book in self.books:
-            if id == book.id:
-                self.books.remove(book)
 
     def get_book_ids(self) -> list[int]:
         ids: list[int] = []
-        for book in self.books:
+        for book in self._books:
             ids.append(book.id)
         return ids
+
+    def _notify_property_changed(self, property_name):
+        for observer in self.observers:
+            observer(property_name)
 
     @staticmethod
     def serialize(obj) -> dict[str]:
